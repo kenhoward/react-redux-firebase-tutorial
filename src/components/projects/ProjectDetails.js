52d14012243d0d7be849ce9aad9ef151eb8ff260
Kenny Howard <ken.chad.howard@gmail.com>
2019-07-02 00:17:26 -0600

    pulling it together

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProjectDetails() {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div classname="card-content">
+                    <span classname="card-title">Project Title</span>
+                    <p>Loreasldkfjalsjdfla;ksjfdlakjsdflja</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by Kendizzle</div>
+                    <div>2nd September, 2am</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
